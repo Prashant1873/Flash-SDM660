Divyanshu-Modi/Atom-X-Kernel

drivers/camera_v2: fix camera issue Miui12-Q
Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

include/uapi/media/msm_camsensor_sdk.h
@@ -325,7 +325,7 @@ struct msm_vcm_id_info_t {
 	enum msm_cci_i2c_master_t cci_i2c_master;
 };
 
-#ifdef CONFIG_XIAOMI_NEW_CAMERA_BLOBS
+#if defined (CONFIG_XIAOMI_NEW_CAMERA_BLOBS) || defined(CONFIG_XIAOMI_CAMERA_MIUI_Q)
 struct msm_lens_id_info_t {
 	unsigned short eeprom_slave_addr;
 	unsigned short lens_id_addr;
@@ -347,20 +347,23 @@ struct msm_camera_sensor_slave_info {
 	enum i2c_freq_mode_t i2c_freq_mode;
 	enum msm_camera_i2c_reg_addr_type addr_type;
 	struct msm_sensor_id_info_t sensor_id_info;
+#ifndef CONFIG_XIAOMI_CAMERA_MIUI_Q	
 	struct msm_vendor_id_info_t vendor_id_info;
 	struct msm_vcm_id_info_t vcm_id_info;
 #ifdef CONFIG_XIAOMI_NEW_CAMERA_BLOBS
 	struct msm_lens_id_info_t lens_id_info;
-#endif
+#endif /*CONFIG_XIAOMI_NEW_CAMERA_BLOBS*/	
+#endif /*CONFIG_XIAOMI_CAMERA_MIUI_Q*/	
 	struct msm_sensor_power_setting_array power_setting_array;
 	unsigned char  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;
 	enum msm_sensor_output_format_t output_format;
 	uint8_t bypass_video_node_creation;
-#ifdef CONFIG_MACH_XIAOMI_WAYNE
+#ifdef CONFIG_XIAOMI_CAMERA_MIUI_Q
 	struct msm_vendor_id_info_t vendor_id_info;
 	struct msm_vcm_id_info_t vcm_id_info;
-#endif
+	struct msm_lens_id_info_t lens_id_info;
+#endif /*CONFIG_XIAOMI_CAMERA_MIUI_Q*/	
 };
 
 struct msm_camera_i2c_reg_array {

Prashant1873/Flash-SDM660

include/uapi/media/msm_camsensor_sdk.h
@@ -329,7 +329,7 @@ struct msm_vcm_id_info_t {
 	enum msm_cci_i2c_master_t cci_i2c_master;
 };
 
-#ifdef CONFIG_MACH_XIAOMI_NEW_CAMERA
+#if defined (CONFIG_MACH_XIAOMI_NEW_CAMERA) || defined(CONFIG_XIAOMI_CAMERA_MIUI_Q)
 struct msm_lens_id_info_t {
     unsigned short eeprom_slave_addr;
     unsigned short lens_id_addr;
@@ -352,18 +352,23 @@ struct msm_camera_sensor_slave_info {
 	enum i2c_freq_mode_t i2c_freq_mode;
 	enum msm_camera_i2c_reg_addr_type addr_type;
 	struct msm_sensor_id_info_t sensor_id_info;
-#ifdef CONFIG_MACH_XIAOMI_SDM660
+#ifndef CONFIG_XIAOMI_CAMERA_MIUI_Q	
 	struct msm_vendor_id_info_t vendor_id_info;
 	struct msm_vcm_id_info_t vcm_id_info;
 #ifdef CONFIG_MACH_XIAOMI_NEW_CAMERA
-    struct msm_lens_id_info_t lens_id_info;
-#endif
-#endif
+	struct msm_lens_id_info_t lens_id_info;
+#endif /*CONFIG_XIAOMI_NEW_CAMERA_BLOBS*/	
+#endif /*CONFIG_XIAOMI_CAMERA_MIUI_Q*/	
 	struct msm_sensor_power_setting_array power_setting_array;
 	unsigned char  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;
 	enum msm_sensor_output_format_t output_format;
 	uint8_t bypass_video_node_creation;
+#ifdef CONFIG_XIAOMI_CAMERA_MIUI_Q
+	struct msm_vendor_id_info_t vendor_id_info;
+	struct msm_vcm_id_info_t vcm_id_info;
+	struct msm_lens_id_info_t lens_id_info;
+#endif /*CONFIG_XIAOMI_CAMERA_MIUI_Q*/	
 };
 
 struct msm_camera_i2c_reg_array {